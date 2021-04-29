3e15eb65ee813bb4011ff23b5657fb5525d8deaa
mengshalitsofi <mengshalitsofi@gmail.com>
2021-04-28 19:32:10 -0700

    commit tree 454dc582d87f7eba2c2725de417b58f8412ec59b

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const element = document.querySelector("p#text");
+    element.innerHTML = "This is really cool!"
+  }); 
